d9e502f9df4549c25372db8143c72f128cc174db
Mara Dusevic <s200494@students.aie.edu.au>
2021-03-10 09:24:59 +1100

    Added graphics engine project

---
+++ aieBootstrap-master/GraphicsProject/GraphicsProjectApp.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Application.h"
+#include <glm/mat4x4.hpp>
+
+class GraphicsProjectApp : public aie::Application {
+public:
+
+	GraphicsProjectApp();
+	virtual ~GraphicsProjectApp();
+
+	virtual bool startup();
+	virtual void shutdown();
+
+	virtual void update(float deltaTime);
+	virtual void draw();
+
+protected:
+
+	// camera transforms
+	glm::mat4	m_viewMatrix;
+	glm::mat4	m_projectionMatrix;
+};
